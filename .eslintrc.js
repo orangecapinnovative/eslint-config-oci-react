8f8b1bbe5d52a0f0433d72906eab48389b4c4d1f
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2022-11-18 11:56:58 +0700

    fix js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: [
     'airbnb',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     '@typescript-eslint/camelcase': 0,
@@ -15,47 +15,47 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        'vars': 'all',
-        'args': 'after-used',
-        'ignoreRestSiblings': true
-      }
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
     ],
     'react/destructuring-assignment': 0,
     'react/function-component-definition': [
       2,
       {
-        'namedComponents': [
+        namedComponents: [
           'arrow-function',
           'function-expression',
-          'function-declaration'
-        ]
-      }
+          'function-declaration',
+        ],
+      },
     ],
     'react/jsx-filename-extension': [
       1,
       {
-        'extensions': [
+        extensions: [
           '.js',
-          '.tsx'
-        ]
-      }
+          '.tsx',
+        ],
+      },
     ],
     'react/jsx-props-no-spreading': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 0,
     'react/jsx-max-props-per-line': ['error', {
-      'maximum': { 'single': 3, 'multiple': 1 }
+      maximum: { single: 3, multiple: 1 },
     }],
     'react/jsx-no-useless-fragment': 0,
     'import/extensions': [
       1,
       {
-        'extensions': [
+        extensions: [
           '.js',
-          '.ts'
-        ]
-      }
+          '.ts',
+        ],
+      },
     ],
     'import/first': 0,
     'import/newline-after-import': 0,
@@ -69,52 +69,52 @@ module.exports = {
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'arrow-body-style': 0,
     'arrow-parens': 0,
-    'camelcase': 0,
+    camelcase: 0,
     'consistent-return': 0,
     'global-require': 0,
     'max-len': [
       1,
       120,
-      2
+      2,
     ],
     'new-cap': [
       2,
       {
-        'capIsNewExceptions': [
-          'Router'
-        ]
-      }
+        capIsNewExceptions: [
+          'Router',
+        ],
+      },
     ],
     'no-confusing-arrow': 0,
     'no-console': [
       'error',
       {
-        'allow': [
+        allow: [
           'warn',
           'error',
-          'info'
-        ]
-      }
+          'info',
+        ],
+      },
     ],
     'no-nested-ternary': 0,
     'no-param-reassign': 0,
     'no-shadow': [
       2,
       {
-        'hoist': 'never'
-      }
+        hoist: 'never',
+      },
     ],
     'no-underscore-dangle': 0,
     'prefer-arrow-callback': 0,
-    'semi': 'error',
-    'strict': [
+    semi: 'error',
+    strict: [
       0,
-      'global'
-    ]
+      'global',
+    ],
   },
-  'globals': {
-    'React': true,
-    'RequestInit': true,
-    'Window': true
-  }
+  globals: {
+    React: true,
+    RequestInit: true,
+    Window: true,
+  },
 };
